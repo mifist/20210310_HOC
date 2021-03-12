04f98008c7aa08eb996cc359d2570c496e1bdd66
romannikiforov <escaro@ukr.net>
2021-03-12 19:35:01 +0200

    ready Content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,20 @@
-import React from "react";
+import {withState} from "recompose"
 
-const Content = () => (
+const enhance = withState("show", "setShow", false)
+
+const Content = ({show, setShow}) => (
   <div className="content">
-    <span className="show-more">Show more</span>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed
-      vulputate odio, sit amet eleifend turpis. Ut faucibus interdum malesuada.{" "}
-    </p>
+    <span onClick={() => setShow(x => !x)} className="show-more">
+      {show ? "Hide more" : "Show more"}
+    </span>
+    {show && (
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed
+        vulputate odio, sit amet eleifend turpis. Ut faucibus interdum
+        malesuada.{" "}
+      </p>
+    )}
   </div>
-);
+)
 
-export default Content;
+export default enhance(Content)
